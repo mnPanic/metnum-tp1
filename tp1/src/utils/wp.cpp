acb2af607c8d6e5470bcee50f019e63bf26a4d11
arehor-ignacio <arehor.ignacio@gmail.com>
2020-04-27 23:48:40 -0300

    add pr suggestions

--- tp1/src/utils/wp.cpp
+++ tp1/src/utils/wp.cpp
@@ -2,46 +2,43 @@
 #include <fstream>
 #include <string>
 
-typedef struct s_entry {
+struct entry {
     int player0;
     int player1;
     int win;
     int date;
-} entry_t;
+};
 
-typedef struct s_info{
+struct info{
     int teams;
     std::vector<entry_t> entires;
-} info_t;
+};
 
 void parseData(std::string file, info_t& info) {
     std::ifstream st (file);
-    if (st.is_open()) {
-        int t, n;
-        info.teams = t;
-
-        st >> t >> n;
-        int p0,p1,g0,g1,d,w;
-        std::vector<entry_t> vec = std::vector<entry_t>();
-
-        for (int i = 0; i < n; i++) {
-            st >> d >> p0 >> g0 >> p1 >> g1;
-            if (g0 > g1) w = p0;
-            else w = p1;
-            entry_t e = entry_t{p0-1, p1-1, w, d};
-            vec.push_back(e);
-        }
-        info.entires = vec;
-    } else {
+    if (!st.is_open()) {
         printf("Error opening file\n");
+        exit(CODE_FAILURE);
+    }
+    int t, n;
+    st >> t >> n;
+
+    info.teams = t;
+    int p0,p1,g0,g1,d,w;
+
+    for (int i = 0; i < n; i++) {
+        st >> d >> p0 >> g0 >> p1 >> g1;
+        if (g0 > g1) w = p0;
+        else w = p1;
+        entry e = entry{p0-1, p1-1, w, d};
+        info.entries.push_back(e);
     }
     st.close();
 }
 
-int main() {
-    info_t partidos;
-    parseData("../tests/test1.in", partidos);
-    double wp[partidos.teams];
+void wp (std::string file, std::vector<double>& results) {
+    info partidos;
+    parseData(file, partidos);
     
     for (int i = 0; i < partidos.teams; ++i) {
         int count = 0;
@@ -53,7 +50,13 @@ int main() {
                     if (partidos.entires[j].win == i) wins++;
                 }
         }
-        wp[i] = (double)wins/(double)count;
+        results[i] = (double)wins/(double)count;
     }
-    return 0;
+}
+
+
+int main() {
+        std::vector<double> res();
+        wp("tests/test1.in", res);
+        return 0;
 }
